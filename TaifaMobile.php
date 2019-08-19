d35b94053300422ed601b6968a94409a2aeb6e45
taifaMobile <53603476+taifaMobile@users.noreply.github.com>
2019-08-19 11:45:23 +0300

    Add files via upload

---
+++ TaifaMobile.php
@@ -0,0 +1,38 @@
+<?php
+class TaifaMobile
+{
+	public function __construct()
+	{ }
+	public function send_sms($recepients, $message, $apiKey, $service_name = NULL)
+	{
+		$plaintext = json_encode(array("message" => $message, "recepients" => $recepients));
+		$encrypted_message = $this->encrypt($plaintext, $apiKey);
+		$data = ["message" => $encrypted_message, "key" => $apiKey, "service_name" => $service_name];
+		$response = $this->curl_function($data, "sms");
+		return $response;
+	}
+
+	public function encrypt($plaintext, $key, $iv = 'w4^dgd$%^62:)dgs')
+	{
+		$encrypted  = openssl_encrypt($plaintext, 'aes-128-cbc', $key, OPENSSL_RAW_DATA, $iv);
+		return bin2hex($encrypted);
+	}
+
+	public function curl_function($data, $end_point)
+	{
+		$url = "http://sms.taifamobile.co.ke/clientapi/" . $end_point . "/";
+		$content = json_encode($data);
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_HEADER, false);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-type: application/json"));
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+
+		$json_response = curl_exec($curl);
+
+		curl_close($curl);
+
+		return $json_response;
+	}
+}
